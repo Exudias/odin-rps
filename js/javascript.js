05b18805c0cbabd3d23641fcfbfa5436a703d483
Exudias <editoo31@gmail.com>
2023-05-16 22:28:43 +0300

    Create playOneRound function

--- js/javascript.js
+++ js/javascript.js
@@ -5,4 +5,52 @@ function getComputerChoice()
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
-console.log(getComputerChoice());
+function playOneRound(playerChoice, computerChoice)
+{
+    computerChoice = computerChoice.toLowerCase();
+    playerChoice = playerChoice.toLowerCase();
+    
+    if (computerChoice === playerChoice)
+    {
+        return "It's a Tie!";
+    }
+
+    if (playerChoice === "rock")
+    {
+        if (computerChoice === "paper")
+        {
+            return "You Lose! Paper beats Rock";
+        }
+        else
+        {
+            return "You Win! Rock beats Scissors";
+        }
+    }
+    else if (playerChoice === "paper")
+    {
+        if (computerChoice === "scissors")
+        {
+            return "You Lose! Scissors beats Paper";
+        }
+        else
+        {
+            return "You Win! Paper beats Rock";
+        }
+    }
+    else if (playerChoice === "scissors")
+    {
+        if (computerChoice === "rock")
+        {
+            return "You Lose! Rock beats Scissors";
+        }
+        else
+        {
+            return "You Win! Scissors beats Paper";
+        }
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+
+console.log(playOneRound(playerSelection, computerSelection));
