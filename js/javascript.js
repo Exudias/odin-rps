ef16a1b31cd4aa1c7795fe470692f1f75affd64d
Exudias <editoo31@gmail.com>
2023-05-19 15:07:00 +0300

    Refactor to support UI

--- js/javascript.js
+++ js/javascript.js
@@ -5,7 +5,7 @@ function getComputerChoice()
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
-function playOneRound(playerChoice, computerChoice)
+function computeWinner(playerChoice, computerChoice)
 {
     playerChoice = playerChoice.toLowerCase();
     
@@ -27,62 +27,6 @@ function playOneRound(playerChoice, computerChoice)
     }
 }
 
-function game()
-{
-    let playerScore = 0;
-    let computerScore = 0;
-
-    for (let i = 0; i < 5; i++)
-    {
-        let playerSelection;
-        while (true)
-        {
-            playerSelection = prompt("Make your pick: ");
-            if (validateInput(playerSelection))
-            {
-                break;
-            }
-            else
-            {
-                console.log("Invalid input!");
-            }
-        }
-        const computerSelection = getComputerChoice();
-
-        switch (playOneRound(playerSelection, computerSelection))
-        {
-            case 0:
-                console.log(`Round ${i + 1}:\nA draw!`);
-                break;
-            case 1:
-                console.log(`Round ${i + 1}:\nYou win! ${capitalize(playerSelection)} beats ${computerSelection.toLowerCase()}.`);
-                playerScore++;
-                break;
-            case -1:
-                console.log(`Round ${i + 1}:\nYou lose! ${capitalize(computerSelection)} beats ${playerSelection.toLowerCase()}.`);
-                computerScore++;
-                break;
-        }
-    }
-
-    let flavorText = "";
-
-    if (playerScore > computerScore)
-    {
-        flavorText = "You win!";
-    }
-    else if (playerScore === computerScore)
-    {
-        flavorText = "It's a tie!";
-    }
-    else
-    {
-        flavorText = "You lose!";
-    }
-
-    console.log(flavorText + ` \nThe score was ${playerScore} : ${computerScore}`);
-}
-
 function capitalize(inputStr)
 {
     return (inputStr.charAt(0).toUpperCase() + inputStr.substring(1).toLowerCase());
@@ -95,4 +39,58 @@ function validateInput(givenInput)
     return choices.includes(givenInput);
 }
 
-game();
+function playRound(playerChoice)
+{
+    const computerChoice = getComputerChoice();
+    let result = computeWinner(playerChoice, computerChoice);
+
+    let output;
+
+    switch (result)
+    {
+        case 0:
+            output = `Round ${roundNumber}:\nA draw!`;
+            break;
+        case 1:
+            output = `Round ${roundNumber}:\nYou win! ${capitalize(playerChoice)} beats ${computerChoice.toLowerCase()}.`;
+            playerScore++;
+            break;
+        case -1:
+            output = `Round ${roundNumber}:\nYou lose! ${capitalize(computerChoice)} beats ${playerChoice.toLowerCase()}.`;
+            computerScore++;
+            break;
+    }
+
+    roundNumber++;
+
+    resultsDiv.textContent = output;
+    scoreDiv.textContent = `Player: ${playerScore} | Computer: ${computerScore}`;
+
+    if (playerScore === 5)
+    {
+        resultsDiv.textContent = "You win!";
+        playerScore = 0;
+        computerScore = 0;
+        roundNumber = 1;
+    }
+    else if (computerScore === 5)
+    {
+        resultsDiv.textContent = "You lose!";
+        playerScore = 0;
+        computerScore = 0;
+        roundNumber = 1;
+    }
+}
+
+const rockButton = document.querySelector(".rock");
+const paperButton = document.querySelector(".paper");
+const scissorsButton = document.querySelector(".scissors");
+
+const resultsDiv = document.querySelector(".results");
+const scoreDiv = document.querySelector(".score");
+
+let playerScore = 0, computerScore = 0, roundNumber = 1;
+
+rockButton.addEventListener("click", () => playRound("rock"));
+paperButton.addEventListener("click", () => playRound("paper"));
+scissorsButton.addEventListener("click", () => playRound("scissors"));
